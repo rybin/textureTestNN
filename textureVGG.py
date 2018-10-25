2312e280ad585bd4e3684b60401593380a5a9d19
rybin <forknispoon@gmail.com>
2018-10-25 10:54:17 +0300

    train 28 classes

--- textureVGG.py
+++ textureVGG.py
@@ -1,10 +1,11 @@
 from keras.preprocessing.image import load_img, img_to_array
 from keras.applications.vgg16 import VGG16
 
-from keras.callbacks import CSVLogger, EarlyStopping, TerminateOnNaN
+from keras.callbacks import CSVLogger, EarlyStopping, TerminateOnNaN, ModelCheckpoint
 
 import numpy as np
 import os
+from time import time
 
 import argparse
 parser = argparse.ArgumentParser(description='Train neural network')
@@ -23,8 +24,6 @@ parser.add_argument('-l', '--log', dest='logfile', action='store',
 
 args = parser.parse_args()
 
-model = VGG16(include_top=True, weights=None, classes=10)
-
 modelfile = args.modelfile
 logfile = args.logfile
 
@@ -41,6 +40,12 @@ textures = ['blanket1',
             'grass1',
             'linseeds1',
             'oatmeal1']
+
+textures = ['blanket1', 'blanket2', 'canvas1', 'ceiling1', 'ceiling2', 'cushion1', 'floor1', 'floor2', 'grass1', 'lentils1', 'linseeds1', 'oatmeal1', 'pearlsugar1', 'rice1', 'rice2', 'rug1', 'sand1', 'scarf1', 'scarf2', 'screen1', 'seat1', 'seat2', 'sesameseeds1', 'stone1', 'stone2', 'stone3', 'stoneslab1', 'wall1']
+
+classes = len(textures)
+
+model = VGG16(include_top=True, weights=None, classes=classes)
 
 images = []
 for i, j in enumerate(textures):
@@ -73,7 +78,7 @@ def gen(batch_size):
         targets = []
         for i in gengen(q, batch_size):
             inputs.append(imgload(folder + i[1]))
-            output = np.zeros(len(textures))
+            output = np.zeros(classes)
             output[int(i[0])] = 1
             targets.append(output)
         yield np.array(inputs), np.array(targets)
@@ -88,16 +93,22 @@ def gen(batch_size):
 callbacks = [
     CSVLogger(filename=logfile),
     # EarlyStopping(monitor='val_acc', mode='max', min_delta=0.01, patience=5, baseline=0.95, verbose=1),
-    # EarlyStopping(monitor='acc', mode='auto', patience=5, verbose=1),
-    TerminateOnNaN()
+    # EarlyStopping(monitor='acc', mode='auto', patience=10, verbose=1),
+    TerminateOnNaN(),
+    ModelCheckpoint('models/modelCheckpoint', monitor='acc', verbose=1, save_best_only=True, mode='auto', period=10)
 ]
 
 model.compile(loss='categorical_crossentropy',
               optimizer='sgd', metrics=['accuracy'])
 
+start = time()
+
 model.fit_generator(gen(batch_size=batch_size),
                     steps_per_epoch=int(len(images) / batch_size),
                     epochs=100,
                     callbacks=callbacks)
+
+finish = time()
+print(f'### Time: {finish-start}')
 
 model.save(modelfile)
